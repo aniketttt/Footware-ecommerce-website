7ac31fce49785bf5360c5eb727a55efd0dca7706
Aniket Sharma <54195664+aniketttt@users.noreply.github.com>
2022-10-16 13:16:38 +0530

    code update

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*---for toggle-----*/
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElems.length; i++){
+    navElems[i].addEventListener("click", function () {
+        navbar.classlist.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+/*-----for header & got to up----*/
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 80) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
